3b56ed6123c81b183cebe1c7a4d5fafe616fa96c
kevin0120 <jingwei1224@163.com>
2022-07-29 15:35:55 +0800

    hello gao si

--- scikit/Dask1/testMachineLearning.py
+++ scikit/Dask1/testMachineLearning.py
@@ -8,38 +8,27 @@ from sklearn.datasets import load_digits, load_wine
 
 if __name__ == '__main__':
     # client = Client(processes=False)  # create local cluster
-    cluster = LocalCluster()
-    client = Client(cluster)
-    # 加载数据
-    X, y = load_wine(return_X_y=True)
-    #
-    # from sklearn.linear_model import LinearRegression
-    # lr = LinearRegression()
-    # Estimators（估算器）
-    from sklearn.linear_model import LogisticRegression
+    client = Client(LocalCluster(n_workers=3, threads_per_worker=7, processes=False))
 
-    # from sklearn.naive_bayes import GaussianNB
-    # 训练样本权重控制
-    # https://blog.csdn.net/weixin_50304531/article/details/109717609
-    # https://blog.csdn.net/qq_43391414/article/details/113144702
-    lr = LogisticRegression(max_iter=10000, class_weight={0: 0.49, 1: 0.49, 2: 0.02})
+    # # 1.分布式
+    # client = Client('127.0.0.1:8786')
+    from sklearn.datasets import make_blobs
 
-    # 拆分数据集--训练+练习
-    from sklearn.model_selection import train_test_split
+    X, y = make_blobs(100, 2, centers=2, random_state=2, cluster_std=1.5)
+    # 实例化高斯模型，并拟合数据：
+    from sklearn.naive_bayes import GaussianNB
 
-    # test_size 联系集所占比例 random_state：是随机数的种子不填或者None每次都会不一样,填了每次都一样
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
-
-    # 根据训练集练习模型
-    import numpy as np
-
-    # sample_weight 可以提高某一条数据的的权重
-    aa = y_train.copy()
-    aa[aa == 0] = 100
-    model = lr.fit(X_train, y_train, sample_weight=aa)
+    model = GaussianNB()
+    model.fit(X, y)
+    # print("参数w={},b={}".format(model.coef_, model.intercept_))
+    # 生成测试数据，并预测：
+    rng = np.random.RandomState(0)
+    Xnew = [-6, -14] + [14, 18] * rng.rand(2000, 2)
+    ynew = model.predict(Xnew)
 
     import dask.array as da
-    d = da.from_array(X_test, chunks=(1, 13))
+
+    d = da.from_array(Xnew, chunks=(1, 2))
     # while True:
     #     for i in range(1, 36):
     #         time.sleep(5)
@@ -49,15 +38,10 @@ if __name__ == '__main__':
     #         # total = client.submit(sum, L)
     #         print(L.result())
     while True:
-        for i in range(1, 36):
-            with joblib.parallel_backend('dask'):
-                # 根据练习集预测
-                predictions = model.predict(d[:i, ...])
-                print(predictions)
-
-                # # 模型评估 根据联系集预测
-                # from sklearn.metrics import classification_report
-                #
-                # print(classification_report(y_test[:i], predictions))
+        for i in range(2000):
+            # with joblib.parallel_backend('dask'):
+            # 根据练习集预测
+            predictions = model.predict(d[:i + 1, ...])
+            print(predictions)
             time.sleep(3)
         time.sleep(3)
