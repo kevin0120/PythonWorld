2746ea599eb6dcf6b2bc110ce5e94cfe33d4c9e0
kevin0120 <jingwei1224@163.com>
2023-03-30 13:17:13 +0800

    d

--- testPath/main.py
+++ testPath/main.py
@@ -1,18 +1,9 @@
 import os
-import subprocess
-import sys
-import win32api
 
 
 def get_usb_path():
-    if os.name == 'nt':  # Windows系统
-        win32api.ShellExecute(0, 'open', 'calc.exe', '', '', 1)
-        drive_list = win32api.GetLogicalDriveStrings().split("\x00")[:-1]
-        win32api.GetDriveType('C:\\')
-        for drive in drive_list:
-            if win32api.GetDriveType(drive) == win32api.DRIVE_REMOVABLE:
-                return drive
-    else:  # Linux系统
+    if os.name != 'nt':  # Windows系统
+        # Linux系统
         with open('/proc/mounts', 'r') as f:
             for line in f.readlines():
                 parts = line.strip().split()
@@ -21,9 +12,18 @@ def get_usb_path():
                         return parts[1]
 
     # 如果没有U盘挂载，则返回可执行文件所在目录
-    return os.path.dirname(os.path.abspath(sys.argv[0]))
+    return os.path.dirname(os.getcwd())
 
 
 if __name__ == '__main__':
     usb_path = get_usb_path()
+    download_path = os.path.join(usb_path, 'download')
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+    filename = 'qqq1.txt'
+    filepath = os.path.join(download_path, filename)
+    with open(filepath, 'w') as f:
+        f.write("this is a example")
+
     print('U盘路径为：', usb_path)
+    print('U盘路径为：', download_path)
